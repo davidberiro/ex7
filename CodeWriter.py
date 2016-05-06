2d5d5a52a1cbbec8915118c94d20f2d4ca4edaff
Omer Rom <omerom88@gmail.com>
2016-05-06 21:50:05 +0300

    didn't do much, beacuse all day freinds push joints to my hands :)

--- CodeWriter.py
+++ CodeWriter.py
@@ -3,5 +3,19 @@ import Parser
 
 
 class CodeWriter:
-    def __init__(self, inputFile):
-        self.parser = Parser(inputFile)
+
+    def __init__(self, filename):
+        # self.parser = Parser(inputFile)
+        self.outFile = open("filename", "wb")
+
+    def setFileName(self,fileName):
+        self.fileName = fileName
+
+    def writeArithmetic (self,command):
+        self
+
+    def WritePushPop (self,command,segment,index):
+        self
+
+    def Close(self):
+        self.outFile.close()
